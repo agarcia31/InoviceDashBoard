e2e328f2e0471df39982308ec708329f38904a0b
Andres Garcia <51844701+agarcia31@users.noreply.github.com>
2023-11-14 22:28:48 -0800

    Learned about how to add basic metadata

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,13 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { Metadata } from 'next';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation'
 
- 
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
